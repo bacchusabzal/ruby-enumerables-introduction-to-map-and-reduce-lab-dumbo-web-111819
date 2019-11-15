430d56592d4b032ed6256c043f7c4ef67e682465
Abzal Bacchus <bacchusabzal1@gmail.com>
2019-11-15 08:25:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,6 +4,9 @@ def map_to_negativize(source_array)
   i = 0 
   new_arr = []
   while i < source_array.length do 
-source_array[]
+new_arr << source_array[i] * -1 
+i += 1 
+end 
+return new_arr
 end 
   
