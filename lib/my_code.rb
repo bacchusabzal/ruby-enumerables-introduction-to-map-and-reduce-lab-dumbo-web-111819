89446f379274ab5a4d2eca1369f2e9b2bc45cc0b
Abzal Bacchus <bacchusabzal1@gmail.com>
2019-11-15 08:47:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -41,7 +41,7 @@ def reduce_to_total(source_array, starting_point)
   counter = 0 
   sum = starting_point
   while counter < source_array.length do 
-    sum = 
+    sum = source_array[counter] + 
   total = [source_array]
   start_point = starting_point
   
