42905c6b981cafc7a4ee217b340af29fbe240c6f
Abzal Bacchus <bacchusabzal1@gmail.com>
2019-11-15 09:02:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -37,7 +37,7 @@ def map_to_square(source_array)
   return new_arr
 end 
 
-def reduce_to_total(source_array, starting_point = 0 )
+def reduce_to_total(source_array)
   i = 0 
   sum = starting_point
   while i < source_array.length do 
