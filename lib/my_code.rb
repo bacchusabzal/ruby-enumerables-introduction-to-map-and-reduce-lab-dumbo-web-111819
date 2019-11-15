3133ae9bd465ee1f9bd85e7f494f480ff96e6c12
Abzal Bacchus <bacchusabzal1@gmail.com>
2019-11-15 08:41:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,8 +38,8 @@ def map_to_square(source_array)
 end 
 
 def reduce_to_total(source_array, starting_point)
-  total = []
-  to[source_array, starting_point]
+  total = reduce_to_total[source_array]
+  
   
   return total
 end 
