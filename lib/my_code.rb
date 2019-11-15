126b8a67927b1f6643edb0fc3d3aedd24842de83
Abzal Bacchus <bacchusabzal1@gmail.com>
2019-11-15 08:56:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -50,7 +50,8 @@ end
 def reduce_to_all_true(source_array)
   i = 0 
   while i < source_array do 
-    if source_array[i] == false || source_array[i] == nil 
+    if (source_array[i] == false || source_array[i] == nil)
+      
 end 
 
 
